f5aea17081d094ee06868834507b01228a29fff4
TJ Hunter <tjhunter@gmail.com>
2012-05-21 17:05:14 -0600

    added markov chain mom jokes.

--- plugins/mom.js
+++ plugins/mom.js
@@ -12,23 +12,9 @@ Plugin = exports.Plugin = function(irc) {
 	this.irc = irc;
 
 	this.irc.addTrigger(this, 'mom', this.mom);
-};
-
-Plugin.prototype.mom = function(msg) {
-	var channelName = msg.arguments[0]
-	  , channel = this.irc.channels[channelName] || false;
-
-	if (!channel) {
-		return;
-	}
-
-	var userList = channel.users // List of all the users in the current channel
-	  , nick = this.irc.parseNick(msg.prefix) // Nick of the user who triggered this callback
-	  , message = msg.arguments[1]
-	  , params = message.split(' ')
-	  , target = params.length > 2 ? params[1].toLowerCase() : false
-	  , num = parseInt(params[2])
-	  , jokes = [
+	this.irc.addTrigger(this, 'momkov', this.momkov);
+	
+	this.jokes = [
 		"your mom is so fat the recursive function calculating her mass causes a stack overflow",
 		"Your mom is so fat she's on both sides of the family.",
 		"Your mom is like a doorknob.... everyone gets a turn!",
@@ -48,6 +34,23 @@ Plugin.prototype.mom = function(msg) {
 		"Yo momma so ugly she gets 364 extra days to dress up for Halloween.",
 		"Yo momma is like a vacuum cleaner.  She sucks, blows, and gets laid in the closet.",
 	];
+};
+
+Plugin.prototype.mom = function(msg) {
+	var channelName = msg.arguments[0]
+	  , channel = this.irc.channels[channelName] || false;
+
+	if (!channel) {
+		return;
+	}
+
+	var userList = channel.users // List of all the users in the current channel
+	  , nick = this.irc.parseNick(msg.prefix) // Nick of the user who triggered this callback
+	  , message = msg.arguments[1]
+	  , params = message.split(' ')
+	  , target = params.length >= 2 ? params[1].toLowerCase() : false
+	  , num = parseInt(params[2])
+	  , jokes = this.jokes;
 	
 	if(target && jokes.length > 0){
 		if (this.irc.botNick.toLowerCase() == target) {
@@ -65,3 +68,120 @@ Plugin.prototype.mom = function(msg) {
 	}	
 	
 };
+
+Plugin.prototype.momkov = function(msg) {
+	var channelName = msg.arguments[0]
+	  , channel = this.irc.channels[channelName] || false;
+
+	if (!channel) {
+		return;
+	}
+
+	var userList = channel.users // List of all the users in the current channel
+	  , nick = this.irc.parseNick(msg.prefix) // Nick of the user who triggered this callback
+	  , message = msg.arguments[1]
+	  , params = message.split(' ')
+	  , target = params.length >= 2 ? params[1].toLowerCase() : false
+	  , num = parseInt(params[2])
+	  , jokes = this.jokes;
+
+	if(target && jokes.length > 0){
+		if (this.irc.botNick.toLowerCase() == target) {
+			channel.send(nick + ", I'm not going to insult my own mother.");
+		} else if (userList.indexOf(target) > -1) {
+			if (num > 0 && num <= jokes.length) {
+				num = num - 1;
+			} else {
+				num = Math.floor(Math.random()*(jokes.length - 1));
+			}
+			channel.send(target + ', ' + this.getMarkov());
+		} else {
+			channel.send(nick + ', ' + target + " isn't here. That's a low blow...");
+		}
+	}	
+	
+};
+
+Plugin.prototype.randomProp = function(obj) {
+	var result
+	  , count = 0;
+
+	for (var prop in obj)
+		if (Math.random() < 1/++count)
+			result = prop;
+
+	return result;
+};
+
+Plugin.prototype.getNext = function(obj) {
+	var sum = 0
+	  , min = 1
+	  , count = 0
+	  , rand
+	  , result;
+
+	for (p in obj) {
+		sum += obj[p];
+	}
+
+	rand = Math.floor(Math.random() * (sum - min + 1) + min);
+	for (var prop in obj) {
+		if (rand <= obj[prop] ) {
+			return prop;
+		} else {
+			rand -= obj[prop];
+		}
+	}
+
+	return result;
+};
+
+Plugin.prototype.getMarkov = function() {
+	var look = 4
+	  , length = 1000
+	  , text = this.jokes.join("\n")
+	  , table = {}
+	  , start;
+	
+	for (var i=0; i < text.length; i++) {
+		var key = text.substring(i, i + look);
+		if (table[key] === undefined) {
+			table[key] = {};
+		}
+	}
+
+	for (var j=0; j < text.length - look; j++) {
+		var key = text.substring(j, j + look);
+		var next = text.substring(j + look, j + look + look);
+
+		if (table[key][next] === undefined) {
+			table[key][next] = 1;
+		} else {
+			table[key][next] += 1;
+		}
+	}
+
+	start = this.randomProp(table);
+	var out = start;
+	var next;
+
+	for (var k=0; k < (length / look); k++) {
+		next = this.getNext(table[start]);
+		if (next) {
+			start = next;
+			out = out + next;
+		} else {
+			start = this.randomProp(table);
+		}
+	}
+
+	var outList = out.split("\n");
+	outList.shift();
+	outList.pop();
+
+	var num = Math.floor(Math.random()*(outList.length - 1));
+	var markov = outList[num];
+
+	return markov;
+};
+
